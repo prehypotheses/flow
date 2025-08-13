e6f070e0f082b263567e034ffc01e97648581259
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-13 19:33:50 +0100

    Update .devcontainer/script.sh

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -2,7 +2,4 @@
 
 BACKEND_STORE_URI="$KEY@$ENDPOINT:$PORT/$DB"
 
-mlflow server \
-  --host 0.0.0.0:5000 \
-  --backend-store-uri "$BACKEND_STORE_URI" \
-  --default-artifact-root "$ARTEFACT_ROOT"
+mlflow server --host 0.0.0.0:5000 --backend-store-uri "$BACKEND_STORE_URI" --default-artifact-root "$ARTEFACT_ROOT"
